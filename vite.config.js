c6ccd835eb267225e497f403b7df6e9050486187
Mirik9724 <fu@fu.flo>
2025-06-22 14:10:49 +1000

    -2.2.3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ev/', // для собственного домена
+  // base: '/ev/', // для собственного домена
   plugins: [react()],
 })
