7154ca0e75ccc76b230c1444d10d02d4e44827f8
Pervovlasenko <foriezsav@gmail.com>
2025-08-11 16:19:11 +1000

    start

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,26 +1,34 @@
 // Main.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import '../src/css/main.css';
 
 function Main() {
+    useEffect(() => {
+        const elements = document.querySelectorAll('.fade-in, .slide-up');
+        elements.forEach(el => {
+            el.style.opacity = '1';
+            el.style.transform = 'translateY(0)';
+        });
+    }, []);
+
     return (
         <div className="App">
             <Header />
-            <main>
-                <div className="center-hor">
-                    <h1>Ender vanilla - Без вайповая ламповая ванилла с элементами роле плэя</h1>
-                    <p><b>Наша главаная цель</b> дать игрокам <em>хорошее настроение</em> и <em>возможность найти друзей</em></p>
+            <main className="main-content">
+                <div className="center-hor glass-container">
+                    <h1 className="fade-in">Ender vanilla - Безвайповая ламповая ванилла с элементами роле плэя</h1>
+                    <p className="slide-up delay-1"><b>Наша главная цель</b> - дать игрокам <em>хорошее настроение</em> и <em>возможность найти друзей</em></p>
 
-                    <h3 className="margin-b">Причины играть на Ender vanilla:</h3>
-                    <ul>
-                        <li>Нет вайпов</li>
-                        <li>Честное комьюнити</li>
-                        <li>Бесплатная проходка</li>
-                        <li>Настоящая Ванилла с улучшенным игровым процессом</li>
-                        <li>Нам уже 2 года и мы не закроемся!</li>
+                    <h3 className="margin-b slide-up delay-2">Причины играть на Ender vanilla:</h3>
+                    <ul className="features-list slide-up delay-3">
+                        <li><span>✦</span> Нет вайпов</li>
+                        <li><span>✦</span> Честное комьюнити</li>
+                        <li><span>✦</span> Бесплатная проходка</li>
+                        <li><span>✦</span> Настоящая Ванилла с улучшенным игровым процессом</li>
+                        <li><span>✦</span> Нам уже 2 года и мы не закроемся!</li>
                     </ul>
-
                 </div>
             </main>
             <Footer />
@@ -28,4 +36,4 @@ function Main() {
     );
 }
 
-export default Main; // Экспортируем компонент по умолчанию
+export default Main;
